8a368d828cbfb6b84a2362d48b54c09dac3bb62e
Henok Yemam <henokadem1@gmail.com>
2019-09-18 09:25:01 -0700

    stats for all teams per league per season

---
+++ sql/awayteam_league_season.sql
@@ -0,0 +1,33 @@
+-- away team per league per season stats
+
+CREATE TABLE awayteam_stats 
+AS(
+    SELECT
+        away.away_team AS awayteam,
+        season,
+        league,
+        SUM(away.a_win) AS away_win,
+        SUM(away.a_loss) AS away_loss,
+        SUM(away.a_draws) AS away_draws,
+        SUM(away.away_team_goal) AS away_goals,
+        SUM(away.home_team_goal) AS away_agst
+        
+        FROM 
+        (SELECT
+        l.name as league,
+        Season, 
+        t1.team_long_name AS home_team,
+        t2.team_long_name AS away_team,
+        away_team_goal, home_team_goal,
+        CASE WHEN away_team_goal > home_team_goal THEN 1 ELSE 0 END AS a_win,
+        CASE WHEN away_team_goal < home_team_goal THEN 1 ELSE 0 END AS a_loss,
+        CASE WHEN away_team_goal = home_team_goal THEN 1 ELSE 0 END AS a_draws
+        FROM Match
+        JOIN Team AS t1
+            ON Match.home_team_api_id = t1.team_api_id 
+        JOIN Team AS t2
+            ON Match.away_team_api_id = t2.team_api_id 
+        JOIN league AS l
+            USING (country_id)) as away
+
+        GROUP BY awayteam,season, league);  
--- sql/practice.sql
+++ sql/practice.sql
@@ -1,24 +1,17 @@
 -- practice sql
 
-SELECT
-        l.name,
-        Season, 
-        t1.team_long_name AS home_team,
-        t2.team_long_name AS away_team,
-        away_team_goal, home_team_goal,
-    CASE WHEN away_team_goal > home_team_goal THEN 1 ELSE 0 END AS a_win,
+SELECT l.name AS league,
+    Season, 
+    t1.team_long_name AS home_team,
+    t2.team_long_name AS away_team,
+    away_team_goal, home_team_goal,
     CASE WHEN home_team_goal > away_team_goal THEN 1 ELSE 0 END AS h_win,
-    CASE WHEN home_team_goal = away_team_goal THEN 1 ELSE 0 END AS draws
+    CASE WHEN home_team_goal < away_team_goal THEN 1 ELSE 0 END AS h_loss,
+    CASE WHEN home_team_goal = away_team_goal THEN 1 ELSE 0 END AS h_draws
     FROM Match
     JOIN Team AS t1
         ON Match.home_team_api_id = t1.team_api_id 
     JOIN Team AS t2
         ON Match.away_team_api_id = t2.team_api_id 
     JOIN league AS l
-        USING (country_id)
-    WHERE Season = '2008/2009' AND l.name = 'England Premier League';
-
-
-    CASE WHEN away_team_goal > home_team_goal THEN 1 ELSE 0 END AS a_win,
-    CASE WHEN away_team_goal < home_team_goal THEN 1 ELSE 0 END AS a_loss,
-    CASE WHEN home_team_goal = away_team_goal THEN 1 ELSE 0 END AS a_draws
+        USING (country_id);
